16b95973d4d2235023b4a8f78652614c27bda009
Caroline Plahter <capl1001@stud.ek.dk>
2026-09-21 10:52:52 +0200

    Tilføj hilsen baseret på tidspunkt

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ function checkDrink() {
     console.log("alkoholfri");
   }
 }
+
+const time = new Date().getHours();
+
+if (time >= 5 && time < 10) {
+  console.log("Godmorgen");
+} else if (time >= 10 && time < 18) {
+  console.log("Goddag");
+} else if (time >= 18 && time < 24) {
+  console.log("Godaften");
+} else {
+  console.log("Godnat");
+}
